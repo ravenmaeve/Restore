a0de87f9a9e70ddfa76d07788cd2bfb58bb27050
ravenmaeve <genaviarodrigo@gmail.com>
2025-05-10 09:00:53 +0800

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,13 +4,14 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 //this is for DbContext
 builder.Services.AddDbContext<StoreContext>(options =>{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+//fixed CORS issue
+builder.Services.AddCors();
 
 /**************************************/
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -29,6 +30,11 @@ var app = builder.Build();
 
 // app.UseAuthorization();
 /**************************************/
+
+app.UseCors(opt =>{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);
---
+++ client/src/app/features/catalog/Catalog.tsx
@@ -0,0 +1,18 @@
+
+import { Product } from "../../models/product";
+import ProductList from "./ProductList";
+
+
+type Props ={
+  products: Product[];
+  
+}
+export default function Catalog({products}:Props) {
+  return (
+    <>
+    <ProductList products={products}/>
+    
+    </>
+     
+  )
+}
